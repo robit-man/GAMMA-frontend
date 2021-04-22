756d517c506a32f29da717c18c960cf3920e348c
pointblank <pointblank.connect@gmail.com>
2021-04-21 19:45:58 -0700

    desc

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -29,12 +29,13 @@
 
     </div>
     <div class="data-wrapper" style="">
-      <div class="data-title" style="">ACCESS THE CONTRACT </div>
+      <p></p>
+      <div class="data-title" style="">GAMMA tokens corrospond to the luminance value of LED pixels. <br><br> Present on the exposed surfaces and livestreamed from a <a href="https://www.geometricspace.ca/launch/1" target="_blank"><i>sattelite to be placed in LEO in JUNE 2022,</i></a>  These pixel's hue <a href="https://kappa-protocol.netlify.app" target="_blank">(KAPPA)</a>, luminosity <a href="https://gamma-protocol.netlify.app" target="_blank">(GAMMA)</a>, X <a href="https://beta-protocol.netlify.app" target="_blank">(BETA)</a> and Y <a href="https://rho-protocol.netlify.app" target="_blank">(RHO)</a> axis locations are acquired in exchange for the afformentioned tokens. <br><br>ACCESS THE CONTRACT </div>
       <div class="data-cylinders  mb-8"  style="">
       <a class="cylinder-module" target="_blank" href="https://www.dextools.io/app/uniswap/pair-explorer/0xe46456153cf0e767528f4a4cd3b613d5c4101e48">
           <img width="64px" height="64px" src="~/@/assets/dextools.png" alt="">
           </a>
-       <a class="cylinder-module" target="_blank" href="https://app.uniswap.org/#/swap">
+       <a class="cylinder-module" target="_blank" href="https://app.uniswap.org/#/swap?outputCurrency=0x1e1eed62f8d82ecfd8230b8d283d5b5c1ba81b55">
           <img width="64px" height="64px" src="~/@/assets/uniswap.png" alt="">
         </a>
         <a class="cylinder-module" target="_blank" href="https://etherscan.io/address/0x1e1eed62f8d82ecfd8230b8d283d5b5c1ba81b55">
