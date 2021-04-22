24c3af8790bc92ed3e4ca3c6d4f12c49ebb15471
pointblank <pointblank.connect@gmail.com>
2021-04-21 20:30:08 -0700

    typos

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -30,7 +30,7 @@
     </div>
     <div class="data-wrapper" style="">
       <p></p>
-      <div class="data-title" style="">GAMMA tokens corrospond to the luminance value of LED pixels. <br><br> Present on the exposed surfaces and livestreamed from a <a href="https://www.geometricspace.ca/launch/1" target="_blank"><i>sattelite to be placed in LEO in JUNE 2022,</i></a>  These pixel's hue <a href="https://kappa-protocol.netlify.app" target="_blank">(KAPPA)</a>, luminosity <a href="https://gamma-protocol.netlify.app" target="_blank">(GAMMA)</a>, X <a href="https://beta-protocol.netlify.app" target="_blank">(BETA)</a> and Y <a href="https://rho-protocol.netlify.app" target="_blank">(RHO)</a> axis locations are acquired in exchange for the afformentioned tokens. <br><br>ACCESS THE CONTRACT </div>
+      <div class="data-title" style="">GAMMA tokens correspond to the luminance value of LED pixels. <br><br>Present on the exposed surfaces and livestreamed from a satellite to be placed in LEO in JUNE 2022, These pixel's hue <a href="https://kappa-protocol.netlify.app" target="_blank">(KAPPA)</a>, luminosity <a href="https://gamma-protocol.netlify.app" target="_blank">(GAMMA)</a>, X-Axis <a href="https://beta-protocol.netlify.app" target="_blank">(BETA)</a> and Y-Axis <a href="https://rho-protocol.netlify.app" target="_blank">(RHO)</a> locations are acquired in exchange for the aforementioned tokens.<br><br>ACCESS THE CONTRACT </div>
       <div class="data-cylinders  mb-8"  style="">
       <a class="cylinder-module" target="_blank" href="https://www.dextools.io/app/uniswap/pair-explorer/0xe46456153cf0e767528f4a4cd3b613d5c4101e48">
           <img width="64px" height="64px" src="~/@/assets/dextools.png" alt="">
