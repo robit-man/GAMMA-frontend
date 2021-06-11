67f56966b44cd7750eec7fdda9ce86b1a6869e08
pointblank <pointblank.connect@gmail.com>
2021-06-11 21:05:48 +0000

    sytling updates

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,8 +2,7 @@
   <div class="full-wrapper">
     
   
-<jj-parallax-card class="" lightColor="transparent" style="margin:auto; width: 512px; height: 512px;" @click="click">
-      <div slot="front">
+      <div class="about-wrap">
          <vh-copy class="para-desktop copy-button"
       :data="'0x1e1eed62f8d82ecfd8230b8d283d5b5c1ba81b55'"
       :confirm_dialog="'buefy'"
@@ -16,7 +15,6 @@
 Present on the exposed surfaces and livestreamed from a satellite to be placed in LEO in JUNE 2022, These pixel's hue (KAPPA), luminosity (GAMMA), X-Axis (BETA) and Y-Axis (RHO) locations are acquired in exchange for the aforementioned tokens.</p>    <br>    <router-link :to="{ name: 'home' }" >
      <img src="~/@/assets/gamma-26.png" style="height:64px;filter:invert(1);" alt="">
     </router-link></div>
-</jj-parallax-card>
 
   </div>
 </template>
